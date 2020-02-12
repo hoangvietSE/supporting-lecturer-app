da28206c11262cfaafadb40d62ae47338ca9d313
HoangVietSE <viethoangtien@gmail.com>
2020-02-12 15:50:35 +0700

    fragment to daggerfragment

--- app/src/main/java/com/soict/hoangviet/supportinglecturer/ui/base/BaseFragment.java
+++ app/src/main/java/com/soict/hoangviet/supportinglecturer/ui/base/BaseFragment.java
@@ -11,7 +11,9 @@ import androidx.fragment.app.Fragment;
 
 import com.soict.hoangviet.supportinglecturer.utils.LoadingUtil;
 
-public abstract class BaseFragment extends Fragment implements BaseView {
+import dagger.android.support.DaggerFragment;
+
+public abstract class BaseFragment extends DaggerFragment implements BaseView {
     private LoadingUtil mLoadingUtil;
 
     @Nullable
